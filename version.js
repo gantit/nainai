6c6dc6bae0efc542dd1fe5ac9ecda9a5329287f0
Johnny <johnny.calvino.contractor@bbva.com>
2025-08-09 15:48:02 +0200

    🔧 Update deployment workflow: generate version info and remove old config files

---
+++ version.js
@@ -0,0 +1,3 @@
+// Archivo generado automáticamente (actualizar en cada despliegue)
+// Sugerencia: un script de build puede sobre-escribir este valor usando la fecha + hash corto de commit
+window.__APP_VERSION__ = "0.1.0-dev";
